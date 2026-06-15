f39dde576e1d6e20d67afe1378aa3ee0328ebe71
jose antonio <joseantonioalmeida217@gmail.com>
2026-06-15 15:51:05 -0300

    Criei a rota POST de users e criei o arquivo especial tests/conftest.py para fixtures.

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,11 +1,11 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
-from fastapi_zero.schemas import Message
+from fastapi_zero.schemas import Message, UserDB, UserPublic, UserSchema
 
-app = FastAPI()
+app = FastAPI(title='Minha Primeira API em FastAPI')
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
@@ -13,14 +13,8 @@ def read_root():
     return {'message': 'Olá, Mundo!'}
 
 
-@app.get('/saudacao', response_class=HTMLResponse)
-def saudacao():
-    return """
-    <html>
-      <head>
-        <title>Nosso olá mundo!</title>
-      </head>
-      <body>
-        <h1> Olá Mundo </h1>
-      </body>
-    </html>"""
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_with_id)
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,13 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fastapi_zero.app import app
-
-
-def test_root_deve_retornar_ola_mundo():
+def test_root_deve_retornar_ola_mundo(client):
     """
     Esse teste tem 3 etapas(AAA)
     - A: Arrange - Arranjo
     - A: Act     - Executa a coisa(o SUT)
     - A: Assert  - Garanta que A é A
     """
-    # arrange
-    client = TestClient(app)
 
     # Act
     response = client.get('/')
@@ -23,13 +17,16 @@ def test_root_deve_retornar_ola_mundo():
     assert response.status_code == HTTPStatus.OK
 
 
-def test_saudacao_retorna_ola_mundo():
-    # arrange
-    client = TestClient(app)
+def test_create_user(client):
+    user = {'username': 'joseteste', 'email': 'joseteste@gmail.com', 'id': 1}
 
-    # act
-    response = client.get('/saudacao')
-
-    # assert
-    assert response.status_code == HTTPStatus.OK
-    assert '<h1> Olá Mundo </h1>' in response.text
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'joseteste',
+            'email': 'joseteste@gmail.com',
+            'password': 'jose321',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == user
