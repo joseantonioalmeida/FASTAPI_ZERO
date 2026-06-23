a7aa279ff9507cbafe7216ecc356d8b3f3b1eda7
jose antonio <joseantonioalmeida217@gmail.com>
2026-06-23 16:41:19 -0300

    Utilizei SqlAlchemy para modelar o meu banco, e fiz teste da tabela User com fixtures e testes.

---
+++ tests/test_db.py
@@ -0,0 +1,25 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
+from fastapi_zero.models import User
+
+
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        new_user = User(
+            username='Teste', email='teste@gmail.com', password='secret'
+        )
+
+        session.add(new_user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'Teste'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'Teste',
+        'email': 'teste@gmail.com',
+        'password': 'secret',
+        'created_at': time,
+    }
