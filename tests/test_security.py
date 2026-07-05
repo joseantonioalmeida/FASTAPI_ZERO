07979098bc4c7cded23188d391646fac4dc47dbd
jose antonio <joseantonioalmeida217@gmail.com>
2026-07-05 20:10:58 -0300

    Teste para token sem sub

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,6 +1,6 @@
 from http import HTTPStatus
 
-from jwt import decode
+from jwt import decode, encode
 
 from fastapi_zero.security import ALGORITHM, SECRET_KEY, create_access_token
 
@@ -18,6 +18,16 @@ def test_jwt():
 def test_jwt_invalid_token(client):
     response = client.delete(
         '/users/1/', headers={'Authorization': 'Bearer token-invalid'}
+    )
+
+    assert response.status_code == HTTPStatus.UNAUTHORIZED
+    assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_token_invalid_not_subject_email(client):
+    token_sem_sub = encode({}, SECRET_KEY, ALGORITHM)
+    response = client.delete(
+        '/users/1/', headers={'Authorization': f'Bearer {token_sem_sub}'}
     )
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
