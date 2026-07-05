4a307ea4c556449541294f298db7f4428c5e13ca
jose antonio <joseantonioalmeida217@gmail.com>
2026-07-05 08:12:30 -0300

    Criei a fixture de token, que vai retornar o token para acessar a aplicação.

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,3 +74,13 @@ def user(session):
     user.clean_password = password  # type:ignore
 
     return user
+
+
+@pytest.fixture
+def token(client, user):
+    response = client.post(
+        '/token/',
+        data={'username': user.email, 'password': user.clean_password},
+    )
+
+    return response.json()['access_token']
