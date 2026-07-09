f6a1844c6f5d5f036353b48996ee8bbf6ef73e63
jose antonio <joseantonioalmeida217@gmail.com>
2026-07-09 16:06:29 -0300

    Testando o token expirado

--- tests/test_security.py
+++ tests/test_security.py
@@ -1,5 +1,6 @@
 from http import HTTPStatus
 
+from freezegun import freeze_time
 from jwt import decode, encode
 
 from fastapi_zero.security import create_access_token
@@ -32,3 +33,23 @@ def test_token_invalid_not_subject_email(client, settings):
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {'detail': 'Could not validate credentials'}
+
+
+def test_token_expired_after_time(client, user):
+    # Parar o tempo nessa data e hora
+    with freeze_time('2026-11-20 12:00:00'):
+        response = client.post(
+            '/auth/token/',
+            data={'username': user.email, 'password': user.clean_password},
+        )
+        assert response.status_code == HTTPStatus.OK
+        token = response.json()['access_token']
+
+    with freeze_time('2026-11-20 12:30:00'):
+        response = client.put(
+            f'/users/{user.id}/',
+            data={'username': user.email, 'password': user.clean_password},
+            headers={'Authorization': f'Bearer {token}'},
+        )
+        assert response.status_code == HTTPStatus.UNAUTHORIZED
+        assert response.json() == {'detail': 'Could not validate credentials'}
